cce084f9d68641b5bb1a1a95787e9c6679fc53ec
DantonMariano <dantonrmariano@gmail.com>
2021-06-08 17:27:14 -0300

    Atualiza Form e Titulo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Todo List</title>
+  <title>Lista de Compras</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" integrity="undefined" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="undefined" crossorigin="anonymous"></script>
@@ -25,11 +25,11 @@
             @csrf
             <div class="mb-3">
               <label for="nome" class="form-label">Receita</label>
-              <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome da Receita">
+              <input required type="text" class="form-control" id="nome" name="nome" placeholder="Nome da Receita">
             </div>
             <div class="mb-3">
               <label for="texto" class="form-label">Ingredientes/Método de preparo:</label>
-              <textarea class="form-control" name="texto" id="texto" rows="3" placeholder="insira a receita aqui!"></textarea>
+              <textarea required class="form-control" name="texto" id="texto" rows="3" placeholder="insira a receita aqui!"></textarea>
             </div>
             <button type="submit" class="btn btn-primary mb-3">Salvar Receita</button>
           </form>
@@ -58,13 +58,14 @@
   <script>
     $(document).ready(() => {
       const card_is_altered = [];
-      
+
       alterar = (id, event) => {
         const selector = `#${id}`;
-        if($(selector).has("form").length){
+
+        if ($(selector).has("form").length) {
           const texto = $(`${selector} input[name='nome']`).val();
           const receita = $(`${selector} textarea[name='texto']`).val();
-          console.log(texto);
+
           $(`${selector} .card-body`).unwrap();
           $(`${selector} input[type='submit']`).remove();
           $(`${selector} br`).remove();
@@ -74,13 +75,13 @@
         } else {
           const texto = $(`${selector} .card-title`).text();
           const receita = $(`${selector} .card-text`).text();
+
           $(`${selector} .card-body`).wrapAll('<form action="/alterar" method="POST">');
           $(`${selector} .card-body`).prepend('@csrf');
           $(`${selector} .card-body`).prepend(`<input type="hidden" name='id' value='${id.slice(id.length - 1)}' />`);
-          $(`${selector} .card-title`).replaceWith(`<input type="text" class="form-control" id="nome" name="nome" placeholder="Nome da Receita" Value='${texto}'></input><br>`);
-          $(`${selector} .card-text`).replaceWith(`<textarea class="form-control" name="texto" id="texto" rows="3" placeholder="insira a receita aqui!">${receita}</textarea>`)
+          $(`${selector} .card-title`).replaceWith(`<input required type="text" class="form-control" id="nome" name="nome" placeholder="Nome da Receita" Value='${texto}'></input><br>`);
+          $(`${selector} .card-text`).replaceWith(`<textarea required class="form-control" name="texto" id="texto" rows="3" placeholder="insira a receita aqui!">${receita}</textarea>`)
           $(`${selector} #cbtns`).append('<input type="submit" value="Alterar" class="btn btn-warning" style="margin-right:auto;" ></input>');
-          console.log(selector);
         }
       }
     });
